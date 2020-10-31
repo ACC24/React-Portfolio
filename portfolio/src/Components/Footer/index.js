4bfee3b8f04ffc43fd7e5d54bda13b70a5b819f4
Andreia <chapman.andreia@gmail.com>
2020-10-30 23:39:20 -0400

    components added

---
+++ portfolio/src/Components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="bg-light py-5">
+            <div className="container"><div className="small text-center text-muted">Copyright © 2020 - Andreia Chapman</div></div>
+        </footer>
+    );
+}
+
+export default Footer;
